befe8afc8c7a7a2bd031856dabd2a261a9465461
Derek Woo <me@derekw.co>
2023-08-27 15:48:52 -0700

    Add back link to About page

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -5,7 +5,8 @@
         </RouterLink>
         <div id="nav">
             <nav>
-                <RouterLink to="/about">About</RouterLink>
+                <RouterLink v-if="onAboutPage" :to="lastPage ? lastPage : '/'">back</RouterLink>
+                <RouterLink v-else to="/about" v-on:click="captureLastPage()">about</RouterLink>
                 <span class="colour-scheme-icon hover-hand" v-on:click="toggleColourScheme" v-if="currentColourScheme">
                     <IconLightMode />
                 </span>
@@ -19,21 +20,30 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import { RouterLink } from 'vue-router';
+import { useRoute, RouterLink } from 'vue-router';
 import LogoMain from '@/components/partials/LogoMain.vue';
 import { colourSchemeStore } from '@/stores/colourSchemeStore';
 import IconDarkMode from '@/components/partials/IconDarkMode.vue';
 import IconLightMode from '@/components/partials/IconLightMode.vue';
 
-import type { ComputedRef } from 'vue';
+import type { Ref, ComputedRef } from 'vue';
 
 const currentColourScheme: ComputedRef<boolean> = ref(computed((): boolean => colourSchemeStore().darkMode));
+const onAboutPage: ComputedRef<boolean> = ref(computed((): boolean => useRoute().path === '/about'));
+const lastPage: Ref<string> = ref('');
 
 function toggleColourScheme() {
     /**
      * Toggle the colour scheme between light and dark mode
      */
     colourSchemeStore().toggleDarkMode();
+};
+
+function captureLastPage() {
+    /**
+     * Capture the last page the user was on
+     */
+    lastPage.value = window.location.pathname.replace("/", "");
 };
 </script>
 
