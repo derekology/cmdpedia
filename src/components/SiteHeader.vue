6c60f9081978f41ffeea4aa45279fd4a1fab72dc
Derek Woo <me@derekw.co>
2023-08-30 02:24:38 -0700

    Add sidebar toggle button to site header

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -13,6 +13,12 @@
                 <span class="colour-scheme-icon hover-hand" v-on:click="toggleColourScheme" v-else>
                     <IconDarkMode />
                 </span>
+                <span class="colour-scheme-icon hover-hand" v-on:click="toggleSidebar" v-if="sidebarOpen">
+                    <IconCloseSidebar />
+                </span>
+                <span class="colour-scheme-icon hover-hand" v-on:click="toggleSidebar" v-else>
+                    <IconOpenSidebar />
+                </span>
             </nav>
         </div>
     </div>
@@ -21,29 +27,40 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import { useRoute, RouterLink } from 'vue-router';
-import LogoMain from '@/components/partials/LogoMain.vue';
 import { colourSchemeStore } from '@/stores/colourSchemeStore';
+import { sidebarStore } from '@/stores/sidebarStore';
+import LogoMain from '@/components/partials/LogoMain.vue';
 import IconDarkMode from '@/components/partials/IconDarkMode.vue';
 import IconLightMode from '@/components/partials/IconLightMode.vue';
+import IconOpenSidebar from '@/components/partials/IconOpenSidebar.vue';
+import IconCloseSidebar from '@/components/partials/IconCloseSidebar.vue';
 
 import type { Ref, ComputedRef } from 'vue';
 
 const currentColourScheme: ComputedRef<boolean> = ref(computed((): boolean => colourSchemeStore().darkMode));
+const sidebarOpen = ref(computed((): boolean => sidebarStore().sidebarOpen));
 const onAboutPage: ComputedRef<boolean> = ref(computed((): boolean => useRoute().path === '/about'));
 const lastPage: Ref<string> = ref('');
 
-function toggleColourScheme() {
+function toggleColourScheme(): void {
     /**
      * Toggle the colour scheme between light and dark mode
      */
     colourSchemeStore().toggleDarkMode();
 };
 
-function captureLastPage() {
+function captureLastPage(): void {
     /**
      * Capture the last page the user was on
      */
     lastPage.value = window.location.pathname.replace("/", "");
+};
+
+function toggleSidebar(): void {
+    /**
+     * Open the sidebar
+     */
+    sidebarStore().toggleSidebar();
 };
 </script>
 
